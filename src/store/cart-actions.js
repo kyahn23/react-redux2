8aae6ef5661914c9fd363b0b7c2cf405c7099848
kyahn23 <kyahn23@gmail.com>
2022-07-31 02:07:35 +0900

    변경여부 추가 / 오타 수정

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -18,13 +18,18 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
           status: "error",
           title: "Error!",
-          message: "Fetching cart data faild.",
+          message: "Fetching cart data failed.",
         })
       );
     }
@@ -44,11 +49,17 @@ export const sendCartData = (cart) => {
     const sendRequest = async () => {
       const response = await fetch(
         "https://react-http-b5104-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
+        }
       );
 
       if (!response.ok) {
-        throw new Error("Sending cart data faild.");
+        throw new Error("Sending cart data failed.");
       }
     };
 
@@ -67,7 +78,7 @@ export const sendCartData = (cart) => {
         uiActions.showNotification({
           status: "error",
           title: "Error!",
-          message: "Sending cart data faild.",
+          message: "Sending cart data failed.",
         })
       );
     }
